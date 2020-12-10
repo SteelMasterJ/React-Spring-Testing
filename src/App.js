65bc3ce9c184d51556b8494787dcf14e7df07ce3
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-12-10 16:37:44 -0500

    More Basic Spring (c3)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
+import React, { Component } from 'react'
 import Component1 from './components/component1'
 import Component2 from './components/component2'
+import Component3 from './components/component3'
+import {Transition} from 'react-spring/renderprops'
 
-function App() {
-  return (
-    <div className="App">
-      <Component1 />
-      <Component2 />
-    </div>
-  );
+class App extends Component {
+  state = {
+    showComponent3: false
+  }
+
+  toggle = e => this.setState({ showComponent3: !this.state.showComponent3 })
+
+  render () {
+    return (
+      <div className="App">
+        <Component1 />
+        <Component2 toggle={this.toggle}/>
+        <Transition 
+          items={this.state.showComponent3}
+          from={{ opacity: 0}}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {show => show && (props => (
+            <div style={props}>
+              <Component3 />
+            </div>
+          ))}
+        </Transition>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/component2.js
+++ src/components/component2.js
@@ -12,12 +12,13 @@ export class component2 extends Component {
             { props => (
                 <div style={props}>
                     <div style={c2Style}>
-                        <h1>Component 1</h1>
+                        <h1>Component 2</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
+                        <button style={btn} onClick={this.props.toggle}>Toggle Component 3</button>
                     </div>
                 </div>
             )}
@@ -30,6 +31,15 @@ const c2Style = {
     background: 'slateblue',
     color: 'white',
     padding: '1.5rem'
-}
+};
+
+const btn = {
+    background: '#333',
+    color: '#fff',
+    padding: '1rem 2rem',
+    border: 'none',
+    textTransform: 'uppercase',
+    margin: '15px 0'
+};
 
 export default component2
